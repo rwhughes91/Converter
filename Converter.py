ce157be7cc6b0e1e9d43bc10e335e10f987d9e8d
rwhughes91 <rwhughes91@gmail.com>
2017-10-23 11:59:32 -0700

    init

---
+++ Converter.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from forex_python.converter import get_rate
+from statistics import mean
+
+
+class Converter:
+    def __init__(self, year, curr):
+        self.year = int(year)
+        self.date_spread = []
+        self.rates = []
+        self.orig_curr = str(curr)
+
+    def date_gen(self, year=False):
+        if year:
+            self.year = year
+        for x in range(1,13):
+            month_ls = []
+            for y in range(1,32):
+                try:
+                    month_ls.append(datetime(self.year,x,y))
+                except ValueError:
+                    pass
+            self.date_spread.append(month_ls)
+
+    def rate_gen(self, to):
+        for x in self.date_spread:
+            monthly_rates = []
+            for y in x:
+                monthly_rates.append(get_rate(self.orig_curr, to, y))
+            self.rates.append(monthly_rates)
+
+    def get_yearly_average(self):
+        monthly_rate_avg = []
+        for x in self.rates:
+            monthly_rate_avg.append(mean(x))
+        yearly_avg = mean(monthly_rate_avg)
+        return yearly_avg
